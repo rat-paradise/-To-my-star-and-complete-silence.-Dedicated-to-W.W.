faaf18c6ee64c16863a7e5e2103ff97622372fed
rat-paradise <first_rat_paradise@hotmail.com>
2025-07-27 01:20:41 +0300

    fix_v2 test_items.py

--- test_items.py
+++ test_items.py
@@ -1,14 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_see_add_to_button(browser):
-    time.sleep(10)
     browser.get(link)
     button = browser.find_elements(By.CSS_SELECTOR, "button[type='submit']")
-    time.sleep(10)
     assert len(button) > 0
     button = button[0]
     assert button.is_displayed(), "Кнопка присутствует, но не видима на странице" 
