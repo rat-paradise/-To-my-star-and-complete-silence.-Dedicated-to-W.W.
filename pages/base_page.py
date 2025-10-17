a2d5d3004fc192c6c52de28f2c75f706ee463baf
rat-paradise <first_rat_paradise@hotmail.com>
2025-10-17 21:34:43 +0300

    обновление - добавлено обработка ошибки

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,18 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
 class BasePage():
-    def __init__(self, browser, url):
-        self.browser = browser  # <-- СВЯЗЫВАЕМ с браузером
-        self.url = url          # <-- ЗАПОМИНАЕМ адрес
-    
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
     def open(self):
         self.browser.get(self.url)  # --> КОМАНДА браузеру: "Иди на этот URL!"
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
